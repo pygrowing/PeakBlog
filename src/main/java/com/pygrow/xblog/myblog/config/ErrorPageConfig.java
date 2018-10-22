507b164df2e55fd0f2c15c9bfc2cdc7201381cfb
pygrowing <1470976@qq.com>
2018-10-22 22:30:18 +0800

    增加错误配置页面

---
+++ src/main/java/com/pygrow/xblog/myblog/config/ErrorPageConfig.java
@@ -0,0 +1,27 @@
+package com.pygrow.xblog.myblog.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+/**
+* @author E先生
+* @version 创建时间：2018年10月22日 下午5:49:44
+* @ClassName ErrorPageConfig
+* @Description 错误页配置
+*/
+@Configuration
+public class ErrorPageConfig {
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+		return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+			@Override
+			public void customize(ConfigurableWebServerFactory factory) {
+				factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,"/errorpage/error-400.html"));
+			}
+		};
+	}
+}
