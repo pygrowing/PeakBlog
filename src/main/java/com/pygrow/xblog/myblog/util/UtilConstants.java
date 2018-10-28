72bfea4d37e1ecd002abcf01e6102f67afb56d78
pygrowing <1470976@qq.com>
2018-10-28 23:33:18 +0800

    新增常量

---
+++ src/main/java/com/pygrow/xblog/myblog/util/UtilConstants.java
@@ -0,0 +1,14 @@
+package com.pygrow.xblog.myblog.util;
+/**
+* @author E先生
+* @version 创建时间：2018年10月26日 下午5:16:50
+* @ClassName 
+* @Description 数据字典
+*/
+public interface UtilConstants {
+
+	/**
+	 * FastDFS服务器地址
+	 */
+	public static final String FASTDFS_URL ="http://192.168.83.129/";
+}
